b30a8953c0efc742c72f2076ebee716dc547aa36
Diony Rosa <dhrosa@gmail.com>
2024-04-29 22:19:22 -0400

    Setup some basic logging, and parse authority information

--- patent_fetcher.py
+++ patent_fetcher.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import re
 from argparse import ArgumentParser
 from collections.abc import Iterator
@@ -6,11 +7,11 @@ from typing import Any, cast
 
 from bs4 import BeautifulSoup, Tag
 from rich import print
-from rich.console import Console
+from rich.logging import RichHandler
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-err_console = Console(stderr=True)
+logger = logging.getLogger(__name__)
 
 
 def fetch_html(url: str) -> str:
@@ -90,33 +91,67 @@ def parse_body(root: Tag) -> Iterator[tuple[str, Any]]:
     assert body
 
     yield "publication_number", dict(parse_publication_number(body))
+    yield "authority", dict(parse_authority(body))
 
 
 def to_snake_case(name: str) -> str:
     return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
 
 
+def parse_properties(tag: Tag) -> Iterator[tuple[str, Any]]:
+    """Generically parse a set of properties associated with a <dt> tag."""
+    for sibling in tag.next_siblings:
+        if not isinstance(sibling, Tag):
+            continue
+        property_name = sibling.get("itemprop")
+        if not property_name:
+            continue
+        if "repeat" in sibling.attrs:
+            logger.debug(f"Skipping repeating itemprop tag: {sibling}")
+            continue
+        content = sibling.get("content")
+        if not content:
+            string = sibling.string
+            if string is None:
+                logger.debug(f"Skipping nested itemprop tag: {sibling}")
+                continue
+            content = string
+
+        yield cast(str, property_name), content
+
+
 def parse_publication_number(body: Tag) -> Iterator[tuple[str, Any]]:
     publication_number = body.find("dt", string=re.compile("Publication number"))
     assert publication_number
+    assert isinstance(publication_number, Tag)
 
-    # Read all generic properties of the publication number
-    for meta in publication_number.find_next_siblings("meta"):
-        assert isinstance(meta, Tag)
-        name = meta["itemprop"]
-        value = meta["content"]
-        yield to_snake_case(cast(str, name)), value
+    yield from parse_properties(publication_number)
 
     # Parse all of the publication number values
     values = list[str]()
     for span in publication_number.find_next_siblings("span"):
         assert isinstance(span, Tag)
-        value = cast(str, span.string).strip()
-        values.append(value)
+        if value := cast(str, span.string).strip():
+            values.append(value)
     yield "values", values
 
 
+def parse_authority(body: Tag) -> Iterator[tuple[str, Any]]:
+    authority = body.find("dt", string=re.compile("Authority"))
+    assert authority
+    assert isinstance(authority, Tag)
+
+    yield from parse_properties(authority)
+
+
 def main() -> None:
+    logging.basicConfig(
+        level="NOTSET",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
+
     parser = ArgumentParser(
         description="Fetch JSON-encoded information about a patent."
     )
