c825dadf2b80eea4bf7f9a8a3659f1e8f2fbb31a
Diony Rosa <dhrosa@gmail.com>
2024-04-29 21:41:42 -0400

    Parse head elements, and publication number info

--- patent_fetcher.py
+++ patent_fetcher.py
@@ -1,10 +1,16 @@
 import json
+import re
 from argparse import ArgumentParser
-from typing import cast
+from collections.abc import Iterator
+from typing import Any, cast
 
+from bs4 import BeautifulSoup, Tag
 from rich import print
+from rich.console import Console
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+err_console = Console(stderr=True)
 
 
 def fetch_html(url: str) -> str:
@@ -37,16 +43,108 @@ def fetch_html(url: str) -> str:
     return cast(str, response["body"])
 
 
+def parse(html: str) -> dict[str, Any]:
+    soup = BeautifulSoup(html, features="html.parser")
+    root = soup.html
+    assert root
+
+    data = dict[str, Any]()
+    data.update(parse_head(root))
+    data.update(parse_body(root))
+    return data
+
+
+def parse_head(root: Tag) -> Iterator[tuple[str, Any]]:
+    head = root.head
+    assert head
+
+    def meta_tag(name: str) -> Tag:
+        return cast(Tag, head.find("meta", attrs={"name": name}))
+
+    def meta_content(name: str) -> str:
+        return cast(str, meta_tag(name)["content"])
+
+    # These fields can just be copied over in a straightforward way without extra processing.
+    for name in (
+        "DC.title",
+        "DC.description",
+        "citation_patent_application_number",
+        "citation_pdf_url",
+        "citation_patent_publication_number",
+    ):
+        yield name.removeprefix("DC."), meta_content(name)
+
+    dates = list[dict[str, Any]]()
+    for meta in head.find_all("meta", attrs={"name": "DC.date"}):
+        dates.append(dict(value=meta.get("content"), type=meta.get("scheme")))
+    yield "dates", dates
+
+    contributors = list[dict[str, Any]]()
+    for meta in head.find_all("meta", attrs={"name": "DC.contributor"}):
+        contributors.append(dict(value=meta.get("content"), type=meta.get("scheme")))
+    yield "contributors", contributors
+
+
+def parse_body(root: Tag) -> Iterator[tuple[str, Any]]:
+    body = root.body
+    assert body
+
+    yield "publication_number", dict(parse_publication_number(body))
+
+
+def to_snake_case(name: str) -> str:
+    return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
+
+def parse_publication_number(body: Tag) -> Iterator[tuple[str, Any]]:
+    publication_number = body.find("dt", string=re.compile("Publication number"))
+    assert publication_number
+
+    # Read all generic properties of the publication number
+    for meta in publication_number.find_next_siblings("meta"):
+        assert isinstance(meta, Tag)
+        name = meta["itemprop"]
+        value = meta["content"]
+        yield to_snake_case(cast(str, name)), value
+
+    # Parse all of the publication number values
+    values = list[str]()
+    for span in publication_number.find_next_siblings("span"):
+        assert isinstance(span, Tag)
+        value = cast(str, span.string).strip()
+        values.append(value)
+    yield "values", values
+
+
 def main() -> None:
     parser = ArgumentParser(
         description="Fetch JSON-encoded information about a patent."
     )
     parser.add_argument(
-        "patent_id", type=str, help="The Google Patent ID to fetch data for."
+        "id_or_url",
+        type=str,
+        help=(
+            "The Google Patent ID to fetch data for. "
+            "If a URL is specified instead, then we fetch data from that URL "
+            "instead of forming the URLs automatically. "
+            "This is useful for debugging (e.g. fetching from a local file:// URL)."
+        ),
     )
+
     args = parser.parse_args()
 
-    print(fetch_html(f"https://patents.google.com/patent/{args.patent_id}/"))
+    url: str
+    if "/" in args.id_or_url:
+        url = args.id_or_url
+    else:
+        patent_id = args.id_or_url
+        url = f"https://patents.google.com/patent/{patent_id}"
+
+    print(parse(fetch_html(url)))
+
+    # with err_console.pager(styles=True):
+    #     print
+    #     err_console.print(fetch_html(url))
 
 
 if __name__ == "__main__":
