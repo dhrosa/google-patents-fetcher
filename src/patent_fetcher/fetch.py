21ec405339583bad96c56b0eabaa8349b8949210
Diony Rosa <dhrosa@gmail.com>
2024-05-01 11:51:02 -0400

    Document fetch_html

--- src/patent_fetcher/fetch.py
+++ src/patent_fetcher/fetch.py
@@ -5,6 +5,23 @@ from selenium.webdriver.chrome.options import Options
 
 
 def fetch_html(url: str) -> str:
+    """Fetch the source HTML of the given URL."""
+
+    # Client requires that we use content that is exactly identical to a human
+    # using "view-source:" as a prefix on a URL in Chrome.
+    #
+    # We can't use the straightforward WebDriver.page_source property, as this
+    # returns the _rendered_ HTML of the page (e.g. this would include all the
+    # syntax highlighting and whitespace transformations performed by the
+    # browser for pretty-printing). A file created by a human using "Save As" on
+    # the view-source: page would produce the correct result. At the time of
+    # writing it did not seem possible to cleanly automate that process without
+    # using experimental Chrome features, as Selenium doesn't have the ability
+    # to interact with the native file dialog.
+    #
+    # Instead, we intercept the network response by enabling performance logging
+    # and using the Chrome Developer Protocol to extract the logged response:
+    # https://stackoverflow.com/a/77065745
     options = Options()
     options.add_argument("headless")
     options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
@@ -12,6 +29,9 @@ def fetch_html(url: str) -> str:
     driver = webdriver.Chrome(options=options)
     driver.get(f"view-source:{url}")
     request_id = ""
+    # Find message containing the full response, which happens in the
+    # loadingFinished event. We use that message to lookup the request ID for
+    # our target response.
     for entry in driver.get_log("performance"):  # type: ignore
         message = json.loads(entry["message"])["message"]
         if message["method"] == "Network.loadingFinished":
@@ -22,14 +42,15 @@ def fetch_html(url: str) -> str:
         raise RuntimeError(
             "Could not find 'Network.loadingFinished' message in browser logs."
         )
-    # Documentation for getResponseBody command:
+    # Fetch the response body:
     # https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-getResponseBody
     response = driver.execute_cdp_cmd(
         "Network.getResponseBody", {"requestId": request_id}
     )
     if response["base64Encoded"]:
-        # We raise an error because the documentation doesn't specify what form
-        # of base64 is used, and we haven't encountered this situation yet.
+        # We raise an error because the documentation doesn't specify what kind
+        # of base64 is used, and we haven't encountered this situation yet in
+        # our manual tests.
         raise NotImplementedError("Encountered base64-encoded content.")
 
     body = response["body"]
