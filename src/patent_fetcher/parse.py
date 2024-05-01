135a3049b3140fc2fb2d52a0cc33b189f0a7b57d
Diony Rosa <dhrosa@gmail.com>
2024-05-01 11:32:51 -0400

    Add more docstrings and comments for parsing code, and trim some unused code.

--- src/patent_fetcher/parse.py
+++ src/patent_fetcher/parse.py
@@ -10,17 +10,22 @@ Node: TypeAlias = dict[str, Any]
 
 logger = getLogger(__name__)
 
+# We define a 'property' as an HTML tag with an 'itemprop' attribute.
+
 
 def tag_string(tag: Tag) -> str:
+    """Human-readable tag information for logging."""
     return f"{tag.name=} {tag.attrs=} {tag.sourceline=}"
 
 
 def has_class(tag: Tag, class_name: str) -> bool:
+    """True if 'class_name' is one of tag's classes"""
     classes = tag.get("class") or []
     return class_name in classes
 
 
 def parse_html(html: str) -> Node:
+    """Parse HTML string"""
     soup = BeautifulSoup(html, features="html.parser")
     root = soup.html
     assert root
@@ -29,7 +34,7 @@ def parse_html(html: str) -> Node:
     assert isinstance(article, Tag)
 
     data = dict[str, Any]()
-    parse_tag(article, data)
+    parse_properties(article, data)
     parse_sections(article, data)
     return data
 
@@ -39,17 +44,24 @@ hack = set[int]()
 START_TAGS = ("dt", "h2")
 
 
-def parse_tag(tag: Tag, current_node: Node) -> None:  # noqa: C901
+def parse_properties(tag: Tag, current_node: Node) -> None:  # noqa: C901
+    """Recursively parse properties.
+
+    We skip over tags that are not related to a property.
+
+    <dt> and <h2> tags are used as labels that delineate properties. Nodes
+    between these tags relate to the previous label.
+    """
     if id(tag) in hack:
         return
     hack.add(id(tag))
-    # logger.debug(f"{tag.name=} {tag.attrs=} {tag.sourceline=}")
     child_node: Node
     if tag.name in START_TAGS:
+        # New label found; begin a new nested node
         label = parse_label(tag)
         logger.debug(f"starting new property: {label}")
         child_node = {}
-        parse_siblings(tag, child_node)
+        parse_siblings_properties(tag, child_node)
         current_node[label] = child_node
         logger.debug(f"ending new property: {label}")
         return
@@ -57,60 +69,64 @@ def parse_tag(tag: Tag, current_node: Node) -> None:  # noqa: C901
     property_name = tag.get("itemprop")
     if not property_name:
         # This tag itself is not a property, but its descendants might be
-        parse_children(tag, current_node)
+        parse_children_properties(tag, current_node)
         return
     assert isinstance(property_name, str)
 
     if tag.name == "section":
-        # Will be handled by parse_sectoins() later
+        # Will be handled by parse_sections() later
         return
 
-    # if parse_special_section(property_name, tag):
-    #     return
-
-    value = property_value(property_name, tag)
+    value = property_value(tag)
 
     if tag.has_attr("repeat"):
+        # "repeat" attribute indicate list-valued properties
         if property_name not in current_node:
             current_node[property_name] = []
-        try:
-            current_node[property_name].append(value)
-        except Exception as e:
-            logger.warning(f"{current_node=} {property_name=}")
-            raise e
+        current_node[property_name].append(value)
     else:
+        # Scalar property
         current_node[property_name] = value
 
 
-def property_value(property_name: str, tag: Tag) -> Any:
+def property_value(tag: Tag) -> Any:
+    """Parse value of a property tag.
+
+    Dependent on the type of tag, the interesting content of the tag"""
     if tag.has_attr("itemscope"):
+        # Nested property
         child_node: Node = {}
-        parse_children(tag, child_node)
+        parse_children_properties(tag, child_node)
         return child_node
     if content := tag.get("content"):
+        # <meta> tags
         return content
     if href := tag.get("href"):
+        # <a> tags
         return href
     if src := tag.get("src"):
+        # <img> tags
         return src
-    if property_name == "content":
-        # Sections such as "abstract" and "description" have nested tags
-        # describing the content
-        return tag.get_text(strip=True)
+    # Otherwise, the text within the node is considered the value
     text = tag.string
     if not isinstance(text, str):
-        logger.warning(f"Skipping tag with no .string: {tag=}")
+        #
+        logger.warning(
+            f"Omitting property value for tag with nested content: {tag_string(tag)}"
+        )
         return None
     return text.strip()
 
 
 def attrs_except_class(tag: Tag) -> FieldIterator:
+    """All HTML attributes of a tag except for 'class'"""
     for key, value in tag.attrs.items():
         if key != "class":
             yield key, value
 
 
 def parse_label(tag: Tag) -> str:
+    """Convert a label (e.g. an h2 tag) into camel case."""
     raw = tag.string
     if not isinstance(raw, str):
         logger.warning("Label tag has no string")
@@ -128,23 +144,29 @@ def parse_label(tag: Tag) -> str:
     return "".join(parts)
 
 
-def parse_children(tag: Tag, current_node: Node) -> None:
+def parse_children_properties(tag: Tag, current_node: Node) -> None:
+    """Parse properties from all child tags"""
     for child in tag.children:
         if not isinstance(child, Tag):
             continue
-        parse_tag(child, current_node)
+        parse_properties(child, current_node)
 
 
-def parse_siblings(tag: Tag, current_node: Node) -> None:
+def parse_siblings_properties(tag: Tag, current_node: Node) -> None:
+    """Parse properties from all sibling tags"""
     for sibling in tag.next_siblings:
         if not isinstance(sibling, Tag):
             continue
         if sibling.name in START_TAGS:
             return
-        parse_tag(sibling, current_node)
+        parse_properties(sibling, current_node)
 
 
 def parse_sections(article: Tag, current_node: Node) -> None:
+    """Parse section tags that are also properties.
+
+    These tags have special structure that is not represented as properties."""
+
     def is_section(tag: Tag) -> bool:
         return tag.name == "section" and tag.has_attr("itemscope")
 
@@ -166,6 +188,7 @@ def parse_sections(article: Tag, current_node: Node) -> None:
 
 
 def parse_abstract(section: Tag) -> FieldIterator:
+    """Parse abstract section"""
     abstract = section.find("abstract")
     assert isinstance(abstract, Tag)
 
@@ -174,6 +197,7 @@ def parse_abstract(section: Tag) -> FieldIterator:
 
 
 def parse_description(section: Tag) -> FieldIterator:
+    """Parse description section"""
     description = section.find(attrs={"class": "description"})
     assert isinstance(description, Tag)
 
@@ -209,6 +233,7 @@ def parse_description(section: Tag) -> FieldIterator:
 
 
 def parse_claims(section: Tag) -> FieldIterator:
+    """Parse claims section"""
     claims_tag = section.find(lambda tag: has_class(tag, "claims"))
     assert isinstance(claims_tag, Tag)
     for key, value in claims_tag.attrs.items():
@@ -226,10 +251,14 @@ def parse_claims(section: Tag) -> FieldIterator:
 
 
 def find_claims(claims_tag: Tag) -> Iterator[Tag]:
-    # Different patent pages have different nesting structure for tags with the
-    # class "claim". So to find the correct tags in a unified way, we find the
-    # tags with the class "claim-text", and return all unique ancestor tags with
-    # the class "claim".
+    """Finds all 'claim' tags nested under the given 'claims' tag.
+
+    Different patent pages have a different nesting structure of 'claim' tags,
+    but only one level of them has the relevant properties. So to find the
+    correct tags in a unified way, we find the tags with the class "claim-text",
+    and return all unique ancestor tags with the class "claim".
+
+    """
     seen_tags = set[int]()
     for text_tag in claims_tag.find_all(lambda t: has_class(t, "claim-text")):
         claim = text_tag.find_parent(lambda t: has_class(t, "claim"))
@@ -241,5 +270,6 @@ def find_claims(claims_tag: Tag) -> Iterator[Tag]:
 
 
 def parse_claim(claim: Tag) -> FieldIterator:
+    """ "Parse a single claim"""
     yield from attrs_except_class(claim)
     yield "text", claim.get_text(strip=True)
