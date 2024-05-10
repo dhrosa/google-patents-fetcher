1eac98dd9244f04a5434792f5b8ce07602c9f614
Diony Rosa <dhrosa@gmail.com>
2024-05-10 15:58:50 -0400

    Only delete dictionary key if it exists.

--- src/google_patents_scraper/parse.py
+++ src/google_patents_scraper/parse.py
@@ -54,7 +54,8 @@ def parse_html(html: str) -> Node:
     # property.
     links = data["links"]
     for name in SPECIAL_SECTION_NAMES:
-        del links[name]
+        if name in links:
+            del links[name]
     parse_special_sections(article, data)
 
     data["info"]["publicationNumber"]["values"] = list(
